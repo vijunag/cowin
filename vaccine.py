db699e74d60431dfbb1b008684b5e4e12eadb9aa
vijunag <vijunag@gmail.com>
2021-05-05 18:11:21 +0530

    Update vaccine.py

--- vaccine.py
+++ vaccine.py
@@ -1,19 +1,24 @@
 import urllib, json
 from twilio.rest import Client
+from datetime import date, timedelta
 
-client = Client() #WhatsApp Client
-resp=urllib.urlopen('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=294&date=06-05-2021')
-data=json.loads(resp.read())
+def CheckSlots(client,day, from_whatsapp_number, to_whatsapp_number):
+    today= date.today().strftime('%d/%m/%Y')
+    resp=urllib.urlopen('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=294&date=%s'%day)
+    data=json.loads(resp.read())
 
-total_avail=0
-hosp_list=[]
-for center in data['centers']:
-    hosp = center['name']
-    for slot in center['sessions']:
-        if slot["available_capacity"] >0 and slot['min_age_limit']<=45:
-            slot['name']=hosp
-            hosp_list.append(slot)
-            total_avail = total_avail + slot['available_capacity']
+    total_avail=0
+    hosp_list=[]
+    for center in data['centers']:
+        if center['fee_type'] == 'Free':
+            continue
+        hosp = center['name']
+        for slot in center['sessions']:
+            if slot["available_capacity"] >0 and slot['min_age_limit']<45:
+                slot['name']=hosp
+                slot['pin']=center['pincode']
+                hosp_list.append(slot)
+                total_avail = total_avail + slot['available_capacity']
 #            print "**********************"
 #            print "Hospital Name: ", hosp
 #            print "Min Age: ", slot['min_age_limit']
@@ -21,25 +26,36 @@ for center in data['centers']:
 #            print "Date: ", slot["date"]
 #            print "Vaccine: ", slot["vaccine"]
 #            print "**********************"
+    if total_avail > 0:
+        client.messages.create(body='%d available: Hurry up'%total_avail,
+                               from_=from_whatsapp_number,
+                               to=to_whatsapp_number)
+    for hosp in hosp_list:
+        print msg%(hosp['name'],hosp['pin'],hosp['date'],hosp['min_age_limit'],hosp['available_capacity'],hosp['date'],hosp['vaccine'])
+        client.messages.create(body=msg%(hosp['name'],hosp['pin'],hosp['date'],hosp['min_age_limit'],hosp['available_capacity'],hosp['date'],hosp['vaccine']),
+                               from_=from_whatsapp_number,
+                               to=to_whatsapp_number)
 
+
+client=Client()
 # this is the Twilio sandbox testing number
-from_whatsapp_number='whatsapp:+xxxxxxxxxxx'
+from_whatsapp_number='whatsapp:xxxxxxxxxx'
 # replace this number with your own WhatsApp Messaging number
-to_whatsapp_number='whatsapp:+xxxxxxxxxxx'
+to_whatsapp_number='whatsapp:xxxxxxxxxxx'
 
 msg="""
-Hospital Name: %s
-Availability: %d
-Date: %s
-Vaccine: %s
+   Hospital Name: %s
+   Hospital Pin: %d
+   Date: %s
+   Age: %d
+   Availability: %d
+   Date: %s
+   Vaccine: %s
 """
 
-if total_avail > 0:
-    client.messages.create(body='%d available: Hurry up'%total_avail,
-                           from_=from_whatsapp_number,
-                           to=to_whatsapp_number)
-for hosp in hosp_list:
-    print msg%(hosp['name'],hosp['available_capacity'],hosp['date'],hosp['vaccine'])
-    client.messages.create(body=msg%(hosp['name'],hosp['available_capacity'],hosp['date'],hosp['vaccine']),
-                           from_=from_whatsapp_number,
-                           to=to_whatsapp_number)
+today = date.today()
+CheckSlots(client, today.strftime('%d-%m-%Y'), from_whatsapp_number, to_whatsapp_number)
+#for x in xrange(1,10):
+#    print "Slots for %s"%today.strftime('%d-%m-%Y')
+#    CheckSlots(client, today.strftime('%d-%m-%Y'), from_whatsapp_number, to_whatsapp_number)
+#    today = today + timedelta(1)
